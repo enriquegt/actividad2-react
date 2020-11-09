c7f3f01bfb8ddec87ae215977526868b31ebd258
Enrique Gonzalez <enriquegonzt@gmail.com>
2020-11-09 01:18:41 -0600

    tarea 1

---
+++ src/components/feed.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Tweet from "./tweet/tweet";
+import {feed} from '../source';
+
+class Feed extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            tweets: feed
+        }
+    }
+
+    render() {
+
+        const {profile, profileName, username, content, interaction, display} = {
+            profile: "",
+            profileName: "",
+            username: "",
+            content: "",
+            interaction: "",
+            display: ""
+        };
+
+        return (
+            <div>
+                
+                {
+                    this.state.tweets.map( tweet => {
+                        return (
+                            <Tweet
+                                profile={tweet.profile}
+                                profileUrl={tweet.profileUrl}
+                                username={tweet.username}
+                                content={tweet.content}
+                                interactionCommnets={tweet.interaction.comments} 
+                                interactionRetweets={tweet.interaction.retweets}
+                                interactionLikes={tweet.interaction.likes}/>
+                        )
+                    }) 
+                }
+            </div>
+        );
+    }
+}
+
+export default Feed;
---
+++ src/components/trends/source.js
@@ -0,0 +1,22 @@
+const feed = [
+    {
+        category: "BBCBreaking",
+        title: `Prince William contracted Covid-19 in April at a similar time to his father Prince Charles, palace sources tell BBC
+        `,
+        noTweets: 590
+    },
+    {
+        category: "NatGeo",
+        title: `What's the most soothing experience you've had in nature? (Bonus points if you have a photo or video)`,
+        noTweets: 10203
+    },
+    {
+        category: "Hipertextual",
+        title: `Cinco recomendaciones, y una sorpresa, para ver en Netflix en noviembre`,
+        noTweets: 34341
+    }
+];
+
+export {
+    feed
+}
